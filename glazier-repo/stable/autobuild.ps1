4640b01f0a91bc64303c2eb5d767cfe59901c5c0
Samuel Melrose <sam.melrose@a1comms.com>
2022-05-24 15:18:54 +0100

    glazier repo update

---
+++ glazier-repo/stable/autobuild.ps1
@@ -0,0 +1,24 @@
+Param(
+    [string]$config_server="http://glazier"
+)
+
+if ($config_server -eq "") {
+  $config_server = "http://glazier"
+}
+
+$Host.UI.RawUI.WindowTitle = 'Glazier'
+$env:LOCALAPPDATA = 'X:\'
+$env:PYTHONPATH = 'X:\glazier\'
+$env:SSL_CERT_FILE = 'X:\glazier-resources\ca-certs.crt'
+Write-Output 'Starting Glazier imaging process...'
+
+# For a full list of Glazier flags, execute `python autobuild.py --helpfull`
+$py_args = @(
+  "X:\glazier\glazier\autobuild.py",
+  "--config_server=$config_server",
+  '--resource_path=X:\glazier-resources',
+  '--glazier_spec_os=windows-stable',
+  '--preserve_tasks=true'
+)
+
+& X:\Python\python.exe $py_args
